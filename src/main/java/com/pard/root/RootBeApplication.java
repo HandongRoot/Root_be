d1ce18f22ba28e258f8200cdb8e3249f3ce9ac08
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2024-08-25 16:10:58 +0900

    🎉 Initial commit, Let's Root!

---
+++ src/main/java/com/pard/root/RootBeApplication.java
@@ -0,0 +1,13 @@
+package com.pard.root;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RootBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pard/root/RootBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pard.root;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
