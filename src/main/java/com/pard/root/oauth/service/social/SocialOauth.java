0053f577b32ec8f8acf0b650e862d0f4910259c1
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2024-12-28 19:33:22 +0900

    ✅ check the provider id before the requestAccessToken

--- src/main/java/com/pard/root/oauth/service/social/SocialOauth.java
+++ src/main/java/com/pard/root/oauth/service/social/SocialOauth.java
@@ -6,8 +6,8 @@ import java.util.Map;
 
 public interface SocialOauth {
     String getOauthRedirectURL();
-    String requestAccessToken(String code);
-    Map<String, Object> getUserInfo(String accessToken);
+    Map<String, Object> requestAccessToken(String code);
+    Map<String, Object> getUserInfo(Map<String, Object> token);
     default SocialLoginType type() {
         if (this instanceof GoogleOauth) {
             return SocialLoginType.GOOGLE;
