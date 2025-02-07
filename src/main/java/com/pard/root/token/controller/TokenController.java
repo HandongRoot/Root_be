bdd54400ee856add60fd3e81f8d07a6ada4e7884
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-02-07 23:14:33 +0900

    ✅ Feat Refreshing AccessToken method

--- src/main/java/com/pard/root/token/controller/TokenController.java
+++ src/main/java/com/pard/root/token/controller/TokenController.java
@@ -1,5 +1,30 @@
 package com.pard.root.token.controller;
 
+import com.pard.root.token.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin
+@RequiredArgsConstructor
+@RequestMapping(value = "/token")
+@Slf4j
 public class TokenController
 {
+    private final TokenService tokenService;
+
+    @PostMapping("/refreshAccessToken")
+    public ResponseEntity<Map<String, Object>> refreshAccessToken(@RequestBody Map<String, String> requestBody) {
+        try {
+            return ResponseEntity.ok(tokenService.refreshAccessToken(requestBody));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
