4d16a8ca45ab1837d2692fa4c02e0a56286afa31
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-02-09 18:54:24 +0900

    ✅ Fix the CustomUserDetail.java

---
+++ src/main/java/com/pard/root/security/entity/CustomUserDetails.java
@@ -0,0 +1,38 @@
+package com.pard.root.security.entity;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+    private final String userId;
+    private final String email;
+    private final String provider;
+    private final List<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(String userId, String email, String provider, Collection<? extends GrantedAuthority> roles) {
+        this.userId = userId;
+        this.email = email;
+        this.provider = provider;
+        this.authorities = roles.stream().toList();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {  // userId를 username으로 사용
+        return userId;
+    }
+}
