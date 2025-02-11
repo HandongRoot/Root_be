6ff077a1dec3425c340b1663d7b24e5916d4486d
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-02-11 20:05:35 +0900

    🔊 Feat: Add the the for checking the authentication in SecurityUtil

--- src/main/java/com/pard/root/config/security/util/SecurityUtil.java
+++ src/main/java/com/pard/root/config/security/util/SecurityUtil.java
@@ -1,15 +1,18 @@
 package com.pard.root.config.security.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.UUID;
 
+@Slf4j
 public class SecurityUtil {
 
     // 현재 로그인한 사용자의 userId 가져오기
     public static UUID getAuthenticatedUserId() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        log.info("authentication: {}", authentication.getPrincipal());
 
         if (authentication == null || "anonymousUser".equals(authentication.getPrincipal())) {
             throw new SecurityException("Unauthorized access");
@@ -22,6 +25,8 @@ public class SecurityUtil {
     public static void validateUserAccess(UUID requestedUserId) {
         UUID tokenUserId = getAuthenticatedUserId();
         if (!tokenUserId.equals(requestedUserId)) {
+            log.info("Unauthorized access attempt detected! Token User ID: {}, Requested User ID: {}",
+                    tokenUserId, requestedUserId);
             throw new SecurityException("Forbidden access");
         }
     }
