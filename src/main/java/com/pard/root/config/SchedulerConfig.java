295cd820bac7f6d6fdb70ab46c3c5c4ff7b162c3
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-02-11 12:00:18 +0900

    🔒 feat scheduled task to delete expired blacklist token
    - Added `@Scheduled` method to remove expired blacklist tokens every hour
    - Implemented `isTokenBlacklisted()` to check if an access token is blacklisted
    - Configured `SchedulerConfig.java` for scheduling tasks

---
+++ src/main/java/com/pard/root/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package com.pard.root.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
--- src/main/java/com/pard/root/token/service/BlacklistedTokenService.java
+++ src/main/java/com/pard/root/token/service/BlacklistedTokenService.java
@@ -3,9 +3,9 @@ package com.pard.root.token.service;
 import com.pard.root.config.component.JwtProvider;
 import com.pard.root.token.entity.BlacklistedToken;
 import com.pard.root.token.repo.BlacklistedTokenRepository;
-import com.pard.root.token.repo.TokenRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -32,7 +32,16 @@ public class BlacklistedTokenService {
         blacklistedTokenRepository.save(blacklistedToken);
     }
 
+    // 해당 Access Token이 blacklist 인지 확인
     public boolean isTokenBlacklisted(String accessToken) {
         return blacklistedTokenRepository.existsByAccessToken(accessToken);
+    }
+
+
+    // System time 기준 매 정각마다 BlackList token 삭제
+    @Scheduled(cron = "0 0 * * * ?")
+    public void deleteBlacklistedTokens() {
+        blacklistedTokenRepository.deleteByExpirationTimeBefore(new Date());
+        log.info("Deleted blacklisted tokens");
     }
 }
