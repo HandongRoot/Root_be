bb2a19b9c743d09f51d125a592a863f6d30c73ae
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-02-27 21:20:33 +0900

    🔗➖ feat(kakao-oauth): Added unlink feature for Kakao OAuth

--- src/main/java/com/pard/root/auth/oauth/service/social/SocialOauth.java
+++ src/main/java/com/pard/root/auth/oauth/service/social/SocialOauth.java
@@ -8,14 +8,15 @@ public interface SocialOauth {
     String getOauthRedirectURL();
     Map<String, Object> requestAccessToken(String code);
     Map<String, Object> getUserInfo(Map<String, Object> token);
+    void unlink(String providerId);
     default SocialLoginType type() {
 //        } else if (this instanceof NaverOauth) {
 //            return SocialLoginType.NAVER;
 //        }
         if (this instanceof KakaoOauth) {
             return SocialLoginType.KAKAO;
-        } else if (this instanceof GoogleOauth) {
-            return SocialLoginType.GOOGLE;
+//        } else if (this instanceof GoogleOauth) {
+//            return SocialLoginType.GOOGLE;
         } else {
             return null;
         }
