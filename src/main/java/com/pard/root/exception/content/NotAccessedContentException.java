5a3cea2b3e0695766fc696cfd315e250f58df5c2
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-03-21 21:53:15 +0900

    🔥 refactor: remove unused ContentException classes

--- src/main/java/com/pard/root/exception/content/NotAccessedContentException.java
+++
@@ -1,9 +0,0 @@
-package com.pard.root.exception.content;
-
-import org.springframework.http.HttpStatus;
-
-public class NotAccessedContentException extends ContentException {
-    public NotAccessedContentException(Long id) {
-        super("You do not own this Content.: " + id, HttpStatus.UNAUTHORIZED);
-    }
-}
--- src/main/java/com/pard/root/exception/content/NotFoundContentException.java
+++
@@ -1,9 +0,0 @@
-package com.pard.root.exception.content;
-
-import org.springframework.http.HttpStatus;
-
-public class NotFoundContentException extends ContentException {
-    public NotFoundContentException(Long id) {
-        super("Content not found:" + id, HttpStatus.NOT_FOUND);
-    }
-}
