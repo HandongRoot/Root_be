0b15895efccde83f52ab1ad8c293673df278c966
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-04-06 16:56:56 +0900

    chore: remove debug logs from social login flow in OauthController

--- src/main/java/com/pard/root/auth/oauth/controller/OauthController.java
+++ src/main/java/com/pard/root/auth/oauth/controller/OauthController.java
@@ -29,7 +29,6 @@ public class OauthController {
     @GetMapping("/{socialLoginType}")
     @Operation(summary = "소셜 로그인 요청 처리 (웹)", description = "사용자가 특정 소셜 로그인 타입(구글, 카카오)으로 로그인 요청을 보내면 해당 요청을 OauthService로 넘겨 처리합니다.")
     public void socialLogin(@PathVariable("socialLoginType") SocialLoginType socialLoginType) {
-        log.info(">> 사용자로부터 SNS 로그인 요청을 받음 :: {} Social Login", socialLoginType);
         oauthService.request(socialLoginType);
     }
 
@@ -56,7 +55,6 @@ public class OauthController {
     @Operation(summary = "소셜 로그인 콜백", description = "소셜 로그인 API 서버(구글, 카카오)로부터 받은 인증 코드를 처리합니다.")
     public ResponseEntity<Map<String, Object>> callback(@PathVariable(name = "socialLoginType") SocialLoginType socialLoginType,
                                         @RequestParam(name = "code") String code) {
-        log.info(">> 소셜 로그인 API 서버로부터 받은 code :: {}", code);
         Map<String, Object> tokens = oauthService.requestAccessToken(socialLoginType, code);
         return ResponseEntity.ok().body(tokens);
     }
